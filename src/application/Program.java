c1fb24e84d6493cb229f222a5ce42b65116f3350
Guilherme Luiz <margilepow1227@gmail.com>
2025-08-26 16:23:53 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import aumentoS.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee emp = new Employee();
+
+        System.out.println("Name: ");
+        emp.name = sc.nextLine();
+        System.out.println("Gross Salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.println("Tax: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println("Employee: " + emp.name + ", $ " + String.format("%.2f", emp.netSalary()));
+
+        System.out.println("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+
+        System.out.println(emp.toString());
+
+    }
+
+}
